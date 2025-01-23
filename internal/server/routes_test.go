9a38fa902a72a7feff92042c55812bf42644e5a7
Natnael Alemayehu <se.natnael.alemayehu@gmail.com>
2025-01-23 14:28:12 +0300

    github fail attempt

--- internal/server/routes_test.go
+++ internal/server/routes_test.go
@@ -20,7 +20,7 @@ func TestHandler(t *testing.T) {
 	if resp.StatusCode != http.StatusOK {
 		t.Errorf("expected status OK; got %v", resp.Status)
 	}
-	expected := "{\"message\":\"Hello World\"}"
+	expected := "{\"message\":\"Home Page\"}"
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		t.Fatalf("error reading response body. Err: %v", err)
